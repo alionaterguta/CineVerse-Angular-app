0dbc0f73fc91c2050a5c5b947bc0fd1ff553fab9
alionaterguta <71376066+alionaterguta@users.noreply.github.com>
2024-04-25 07:23:30 +0200

    Update user-login-form.component.ts

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { UserLoginService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
@@ -16,23 +17,28 @@ export class UserLoginFormComponent implements OnInit {
   constructor(
     public fetchApiData: UserLoginService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    private router: Router
   ) {}
+
   ngOnInit(): void {}
 
   loginUser(): void {
     this.fetchApiData.userLogin(this.userData).subscribe(
       (result) => {
+        console.log(result);
         this.dialogRef.close();
-        this.snackBar.open(result, 'OK', {
+        this.snackBar.open('User Login successful', 'OK', {
           duration: 2000,
         });
         localStorage.setItem('currentUser', JSON.stringify(result.user));
         localStorage.setItem('token', result.token);
+        this.router.navigate(['movies']);
       },
+
       (result) => {
         this.snackBar.open(result, 'NOT OK', {
-          duration: 9000,
+          duration: 2000,
         });
       }
     );
