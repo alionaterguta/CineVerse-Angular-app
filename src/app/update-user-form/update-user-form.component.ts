24767524c00004c25d6bf883d17813910a98fecd
alionaterguta <71376066+alionaterguta@users.noreply.github.com>
2024-04-30 14:46:36 +0200

    Generated TypeDoc documentation

--- src/app/update-user-form/update-user-form.component.ts
+++ src/app/update-user-form/update-user-form.component.ts
@@ -2,6 +2,10 @@ import { Component, Input, OnInit } from '@angular/core';
 import { UpdateInfoUserService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+/**
+ * Component for updating user information.
+ * Allows users to modify their username, password, email, and birthdate.
+ */
 @Component({
   selector: 'app-update-user-form',
   templateUrl: './update-user-form.component.html',
@@ -15,13 +19,25 @@ export class UpdateUserFormComponent implements OnInit {
     Email: '',
     Birthdate: Date,
   };
+  /**
+   * Constructs the UpdateUserFormComponent.
+   * @param fetchApiData Service for updating user information
+   * @param snackBar Service for displaying snack bar notifications
+   */
   constructor(
     public fetchApiData: UpdateInfoUserService,
     public snackBar: MatSnackBar
   ) {}
 
+  /**
+   * Lifecycle hook that is called after data-bound properties of a directive are initialized.
+   */
   ngOnInit(): void {}
 
+  /**
+   * Updates the user information.
+   * Calls the fetchApiData service to update the user data and displays a success or failure message accordingly.
+   */
   updateUser(): void {
     this.fetchApiData.updateInfoUser(this.userData).subscribe(
       (resp: any) => {
--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -3,6 +3,10 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { UserRegistrationService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+/**
+ * Component for user registration form.
+ * Allows users to input their username, password, email, and birthdate to register.
+ */
 @Component({
   selector: 'app-user-registration-form',
   templateUrl: './user-registration-form.component.html',
@@ -15,19 +19,32 @@ export class UserRegistrationFormComponent implements OnInit {
     Email: '',
     Birthdate: Date,
   };
+
+  /**
+   * Constructs the UserRegistrationFormComponent.
+   * @param fetchApiData Service for user registration
+   * @param dialogRef Reference to the dialog that hosts the registration form
+   * @param snackBar Service for displaying snack bar notifications
+   */
   constructor(
     public fetchApiData: UserRegistrationService,
     public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
     public snackBar: MatSnackBar
   ) {}
 
+  /**
+   * Lifecycle hook that is called after data-bound properties of a directive are initialized.
+   */
   ngOnInit(): void {}
-  // This is the function responsible for sending the form inputs to the backend
+
+  /**
+   * Registers a new user.
+   * Calls the fetchApiData service to register the user and displays a success or failure message accordingly.
+   */
   registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe(
       (result) => {
-        // Logic for a successful user registration goes here! (To be implemented)
-        this.dialogRef.close(); // This will close the modal on success!
+        this.dialogRef.close();
         this.snackBar.open(result, 'OK', {
           duration: 2000,
         });
